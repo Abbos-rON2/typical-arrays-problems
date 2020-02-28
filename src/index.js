703ce4f171bdd015279839d08c7247298b723a4a
Abbos <abbos6264258@gmail.com>
2020-02-28 22:53:34 +0500

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if (array != undefined && array.length > 0) {
+        array = array.sort(function (a, b) {
+            return a - b
+        });
+        return array[0];
+    } else return 0;
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array != undefined && array.length > 0) {
+        array = array.sort(function (a, b) {
+            return b - a
+        });
+        return array[0];
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let result = 0;
+    if (array != undefined && array.length > 0) {
+        array.forEach(item => {
+            result += parseInt(item);
+        });
+        result = result / array.length
+    }
+    return result;
+
 }
